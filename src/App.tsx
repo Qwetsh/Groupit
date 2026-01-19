f48a23aec837cf29509e5d877146503221365610
Qwetsh <tomicharles@gmail.com>
2026-01-19 19:55:42 +0100

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<DashboardPage />} />
